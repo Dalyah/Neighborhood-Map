51eb5a59fd2829c0833e16b1ae26864a9738d378
DalyahAljamal <dalyah_se@outlook.com>
2019-05-21 07:16:46 +0300

    Add FourSquare API

--- js/app.js
+++ js/app.js
@@ -55,8 +55,28 @@ var markers = [];
       // Check to make sure the infowindow is not already opened on this marker.
       if (infowindow.marker != marker) {
         infowindow.marker = marker;
-        infowindow.setContent('<div>' + marker.title + '</div>');
+        var infowindowTitle = '<div>' + marker.title + '</div>';
         infowindow.open(map, marker);
+        var fourSquareContent ="";
+
+        // using FourSquare Api to add more content
+        var CLIENT_ID ="DH01K1OPPGDSEFMMWPYKYDYB0X0GY1XKMUBHN0YSNFQWKZD4"
+        var CLIENT_SECRET="T3ESFKYUUTRAZPQGZG5IGOWKENS4QMSPVN21L353JXS55Q1H"
+        $.getJSON("https://api.foursquare.com/v2/venues/explore?client_id="
+        +CLIENT_ID+"&client_secret="+CLIENT_SECRET+"&v=20180323&limit=1&ll="
+        + marker.position.lat() + "," + marker.position.lng()+"&query="
+        +marker.title).done(function(result) {
+            if (result.response.groups[0].items[0]) {
+              console.log(result)
+              var category = result.response.groups[0].items[0].venue.categories[0].name
+            }
+        }).fail(function() {
+            fourSquareContent = fourSquareContent + '<div> Cannot load Foursquare content</div>';
+        }).always(function() {
+            var attribution = '<div> Powered by Foursquare!</div>';
+            infowindow.setContent(infowindowTitle + fourSquareContent + attribution);
+        });
+
         // Make sure the marker property is cleared if the infowindow is closed.
         infowindow.addListener('closeclick',function(){
           infowindow.setMarker = null;
