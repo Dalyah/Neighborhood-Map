393abf0c326cfd0f33fa94ee5c75de7c4b7c2b66
DalyahAljamal <dalyah_se@outlook.com>
2019-05-21 08:03:05 +0300

    Format Check

--- js/app.js
+++ js/app.js
@@ -38,7 +38,6 @@ var largeInfowindow;
         });
     }
 
-
     // function to handel map loading Error
     function handlError(){
       alert('Error loading Google Map');
@@ -52,6 +51,8 @@ var largeInfowindow;
          marker.setAnimation(google.maps.Animation.BOUNCE);
        }
      }
+
+     // show marker infowindow
      function populateInfoWindow(marker, infowindow) {
       // Check to make sure the infowindow is not already opened on this marker.
       if (infowindow.marker != marker) {
@@ -61,16 +62,16 @@ var largeInfowindow;
         var fourSquareContent ="";
 
         // using FourSquare Api to add more content
-        var CLIENT_ID ="DH01K1OPPGDSEFMMWPYKYDYB0X0GY1XKMUBHN0YSNFQWKZD4"
-        var CLIENT_SECRET="T3ESFKYUUTRAZPQGZG5IGOWKENS4QMSPVN21L353JXS55Q1H"
+        var CLIENT_ID = "DH01K1OPPGDSEFMMWPYKYDYB0X0GY1XKMUBHN0YSNFQWKZD4";
+        var CLIENT_SECRET= "T3ESFKYUUTRAZPQGZG5IGOWKENS4QMSPVN21L353JXS55Q1H";
         $.getJSON("https://api.foursquare.com/v2/venues/explore?client_id="
-        +CLIENT_ID+"&client_secret="+CLIENT_SECRET+"&v=20180323&limit=1&ll="
-        + marker.position.lat() + "," + marker.position.lng()+"&query="
-        +marker.title).done(function(result) {
+        + CLIENT_ID + "&client_secret=" + CLIENT_SECRET + "&v=20180323&limit=1&ll="
+        + marker.position.lat() + "," + marker.position.lng() + "&query="
+        + marker.title).done( function(result) {
             if (result.response.groups[0].items[0]) {
               console.log(result)
               var category = result.response.groups[0].items[0].venue.categories[0].name
-              fourSquareContent = fourSquareContent + '<div>'+category+'</div>';
+              fourSquareContent = fourSquareContent + '<div>'+ category + '</div>';
             }
         }).fail(function() {
             fourSquareContent = fourSquareContent + '<div> Cannot load Foursquare content</div>';
